c7363b14886ae2024a513a7d4eed8741e408162c
Alejandro <alpachonr@unal.edu.co>
2023-10-26 13:05:22 -0500

    sumador con numeros negativos y todo

--- lab_sum/sum_res_carry/full_sumres.v
+++ lab_sum/sum_res_carry/full_sumres.v
@@ -1,20 +1,46 @@
-module full_sumres (
-	input [3:0]in_a,
-	input [3:0] in_b,
-	input  op,
-	output out_cy0,
-	output [3:0] out_s2
-	);
+module full_sumres (in_a,in_b,op,out_cy0,out_s2,sign0);
+	input [3:0]in_a;
+	input [3:0] in_b;
+	input  op;
+	output out_cy0;
+	output [3:0] out_s2;
+	output sign0;
+	reg [3:0]temporal;
+	reg [3:0]t_a;
+	reg [3:0]t_b;
+	reg sign;
+	always @ (in_a,in_b,op)
+		begin
+			sign=0;
+			t_a=in_a;
+			t_b=in_b;
+			if (op ==1)
+				begin
+					if (in_a < in_b) 
+						begin
+							temporal= in_a;
+							t_a =t_b;
+							t_b= temporal;
+							sign= 1;
+						end
+					else 
+						sign= 0;
+				end
+			else
+				sign=0;
+		end			
+	
+	assign sign0=sign;
 	wire cable0;
 	wire cable1;
 	wire cable2;
 	wire [3:0]in_bm; 
-	scalarxor ope(in_b,op,in_bm);
+	scalarxor ope(t_b,op,in_bm);
 
-	sumres sumres0(in_a[0],in_bm[0],op,out_s2[0],cable0);
-	sumres sumres1(in_a[1],in_bm[1],cable0,out_s2[1],cable1);
-	sumres sumres2(in_a[2],in_bm[2],cable1,out_s2[2],cable2);
-	sumres sumres3(in_a[3],in_bm[3],cable2,out_s2[3],out_cy0);
+	sumres sumres0(t_a[0],in_bm[0],op,out_s2[0],cable0);
+	sumres sumres1(t_a[1],in_bm[1],cable0,out_s2[1],cable1);
+	sumres sumres2(t_a[2],in_bm[2],cable1,out_s2[2],cable2);
+	sumres sumres3(t_a[3],in_bm[3],cable2,out_s2[3],out_cy0);
 
 
 
